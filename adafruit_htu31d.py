df552f04da02406734f1d7e42a0c584fd2e08b8b
lady ada <limor@ladyada.net>
2021-01-18 15:35:30 -0500

    first commit

---
+++ adafruit_htu31d.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 ladyada for Adafruit
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_htu31d`
+================================================================================
+
+Python library for TE HTU31D temperature and humidity sensors
+
+
+* Author(s): ladyada
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+* Adafruit HTU31D breakout: https://www.adafruit.com/product/4832
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+
+"""
+
+# imports
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_HTU31D.git"
